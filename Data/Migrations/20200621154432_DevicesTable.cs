c0e7aea86437f3175dee2167dcf63e108b339322
Herb1691 <hwright_1691@hotmail.com>
2020-06-21 14:04:37 -0500

    Added Model and Controller

---
+++ Data/Migrations/20200621154432_DevicesTable.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace capstone.Data.Migrations
+{
+    public partial class DevicesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SystemType = table.Column<string>(nullable: true),
+                    Maker = table.Column<string>(nullable: true),
+                    SystemName = table.Column<string>(nullable: true),
+                    DatePurchased = table.Column<DateTime>(nullable: false),
+                    IsAssigned = table.Column<bool>(nullable: false),
+                    Organization = table.Column<string>(nullable: true),
+                    Department = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Devices_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_UserId",
+                table: "Devices",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 1, "Jon", "Smith" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 2, "Bobby", "Miller" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 3, "Sarah", "Brooks" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 1, "Sam", "Smith" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 2, "Tom", "Miller" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 3, "Mary", "Brooks" });
+        }
+    }
+}
